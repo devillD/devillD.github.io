64cc6d581dc869e4007719ff04f09e64d495bb03
Devill Don <53294521+devillD@users.noreply.github.com>
2022-05-14 17:26:37 +0600

    Update tsrc.js

--- tsrc/tsrc.js
+++ tsrc/tsrc.js
@@ -8,7 +8,7 @@ async function get_torrent() {
 
   const query = document.getElementById("query").value;
   if(query !== ''){
-    const api = "https://dev.abirxo.cf/" +siteName +"/" +query;
+    const api = "https://api-zero.onrender.com/" +siteName +"/" +query;
     
     const html = await axios.get(api);
     const torrents = await html.data;
