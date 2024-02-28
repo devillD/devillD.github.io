4fb39ec944a05a0341677789aee0ba482def66b9
Devill Don <devilldon5@gmail.com>
2024-02-28 23:40:08 +0600

    Update tsrc.js

--- tsrc/tsrc.js
+++ tsrc/tsrc.js
@@ -7,7 +7,7 @@ async function getTorrent() {
 
     const query = document.getElementById("query").value;
     if (query !== '') {
-        const api = "https://api.api-zero.workers.dev/" + siteName + "/" + query;
+        const api = "https://api-devilld.koyeb.app/" + siteName + "/" + query;
 
         try {
             const response = await fetch(api);
